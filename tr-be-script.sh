d93e53441c14fc89326ffcb98c74ed03d70a5701
Arnav Gosain <gosain.arnav@gmail.com>
2014-06-22 00:20:42 +0530

    Schedtool much

--- tr-be-script.sh
+++ tr-be-script.sh
@@ -140,6 +140,7 @@ libgl1-mesa-dev g++-multilib mingw32 tofrodos \
 python-markdown libxml2-utils xsltproc zlib1g-dev:i386 \
 android-tools-adb android-tools-fastboot libcloog-isl-dev \
 texinfo gcc-multilib \
+schedtool \
 sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 if [ ${SKIP} = 1 ]; then
